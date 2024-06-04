17447ba6e328605cc5241829cce22d8e43ddf8d2
TLLRSK <tullarisk@gmail.com>
2024-06-04 22:39:44 +0200

    add: swiper library

--- src/components/questionsCarousel/QuestionsCarousel.vue
+++ src/components/questionsCarousel/QuestionsCarousel.vue
@@ -1,24 +1,35 @@
 <template>
-    <div class="swiper">
-        <ul class="questions-carousel">
-            <li v-for="question in questions" :key="question">
-                <button>{{question }}</button>
-            </li>
-        </ul>
+    <div>
+      <swiper-container
+        :slides-per-view="3"
+        navigation="true"
+      >
+        <swiper-slide v-for="(question, index) in questions" :key="index">
+          <button>{{ question }}</button>
+        </swiper-slide>
+      </swiper-container>
     </div>
-</template>
-
-<script>
-    export default {
-        data() {
-            return {
-                questions: [
-                    "Qué es un chatbot",
-                    "Para qué sirve",
-                    "Qué es 1milionbot",
-                    "Qué es una IA",
-                ]
-            }
-        }
+  </template>
+  
+  <script>
+  import { register } from 'swiper/element/bundle';
+  import { ref } from 'vue';
+  
+  // Registrar los elementos personalizados de Swiper
+  register();
+  
+  export default {
+    setup() {
+      const questions =[
+        "Qué es un chatbot",
+        "Para qué sirve",
+        "Qué es 1milionbot",
+        "Qué es una IA"
+      ];
+  
+      return {
+        questions
+      };
     }
-</script>
+  };
+  </script>
