4d19058d3ea1ff163db5675f3697650dafd51d40
TLLRSK <tullarisk@gmail.com>
2024-06-04 17:07:45 +0200

    add: basic message management in ChatContainer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="chat-window">
     <Nav></Nav>
-    <div class="messages">
-      <message-image-card></message-image-card>
-    </div>
+    <chat-container></chat-container>
     <chat-questions-carousel/>
     <message-input/>
   </div>
 </template>
 
 <script>
+import ChatContainer from './components/chatContainer/ChatContainer.vue';
 import ChatQuestionsCarousel from './components/chatContainer/ChatQuestionsCarousel.vue';
 import MessageInput from './components/messageInput/MessageInput.vue';
 import MessageImageCard from './components/messages/MessageImageCard.vue';
@@ -18,12 +17,14 @@ import Nav from './components/nav/Nav.vue';
 export default {
   components: {
     Nav,
+    ChatContainer,
     MessageImageCard,
-    MessageInput,
     ChatQuestionsCarousel,
+    MessageInput,
   },
   data() {
     return {
+      messagesLog: []
     };
   },
   methods: {
--- src/components/chatContainer/ChatContainer.vue
+++ src/components/chatContainer/ChatContainer.vue
@@ -1,11 +1,29 @@
 <template>
-    <main>
-        
-    </main>
+    <ul>
+        <li :class="['message', {'message-user': message.author === 'user', 'message-chatbot': message.author === 'chatbot' }]" v-for="message in messages" :key="message">
+            {{ message.content }}
+        </li>
+    </ul>
 </template>
 
 <script>
+    export default {
+        props: {
+            messagesLog: {
+                type: Array,
+            }
+        },
+        data() {
+            return {
+                messages: [
+                    {
+                        author: "user",
+                        content: "lorem ipsum lorem.",
+                    }
+                ],
+            }
+        }
+    }
 </script>
-
 <style>
 </style>
